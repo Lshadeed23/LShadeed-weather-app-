c7e02e44c39a7a57af7f4f9a47b7000c9459ef35
Lshadeed23 <149407615+Lshadeed23@users.noreply.github.com>
2023-12-31 18:48:47 -0600

    readded axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+function refreshWeather(response) {
+    console.log(response.data);
+ }
+
+function searchCity(city) {
+    let apiKey = "e7acafab59o2a05b63830a8t5f4aa341";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
-
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
